6a9fd937acf62b1fff0ff88f277c4c4ddde155f1
PrinceOfPuppers <50126753+PrinceOfPuppers@users.noreply.github.com>
2020-11-17 10:59:11 -0500

    Update winInstall.py

--- winInstall.py
+++ winInstall.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     ver =f"cp{version_info[0]}{version_info[1]}"
 
     if platform == 'win32' or platform =='amd64':
-        traitsURL = f"https://download.lfd.uci.edu/pythonlibs/z2tqcw5k/traits-6.1.1-{ver}-{ver}-{platform}.whl"
-        vtkURL = f"https://download.lfd.uci.edu/pythonlibs/z2tqcw5k/VTK-9.0.1-{ver}-{ver}-{platform}.whl"
+        traitsURL = f"https://download.lfd.uci.edu/pythonlibs/z4tqcw5k/traits-6.1.1-{ver}-{ver}-{platform}.whl"
+        vtkURL = f"https://download.lfd.uci.edu/pythonlibs/z4tqcw5k/VTK-9.0.1-{ver}-{ver}-{platform}.whl"
         installManual(traitsURL)
         installManual(vtkURL)
         installRest()
